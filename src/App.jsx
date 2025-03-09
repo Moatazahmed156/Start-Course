0e5f1e7c4c613103273c039ea350c4e2c26efc21
Moatazahmed156 <moatazahmed696@gmail.com>
2025-03-10 00:28:22 +0200

    GG

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,7 @@ import "./App.css";
 import Home from "./Pages/Home";
 import CoursePage from "./Pages/ScoreBoard";
 import NotFound from "./Pages/NotFound";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  BrowserRouter,
-} from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
